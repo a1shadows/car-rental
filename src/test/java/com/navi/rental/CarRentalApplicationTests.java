8225e28b528121ae3508319379680974bb6b850d
adityalahiri <aditya.lahiri2009@gmail.com>
2022-07-03 23:19:44 +0530

    todo: no test cases

--- src/test/java/com/navi/rental/CarRentalApplicationTests.java
+++ src/test/java/com/navi/rental/CarRentalApplicationTests.java
@@ -3,6 +3,9 @@ package com.navi.rental;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/***
+ * APOLOGIES: DIDN'T GET THE TIME TO WRITE TEST CASES...
+ ***/
 @SpringBootTest
 class CarRentalApplicationTests {
 
